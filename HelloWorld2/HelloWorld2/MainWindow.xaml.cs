034ad80ef7d7ac622d2122458cf78ef5faa873d4
krichpapath <krichpapath@gmail.com>
2021-04-21 11:13:26 +0700

    first comit

---
+++ HelloWorld2/HelloWorld2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(Income.Text);
+            double y = double.Parse(Expenditure.Text);
+            double z = double.Parse(IWant.Text);
+            double save = z / (x - y);
+            Saving.Text = (save.ToString());
+
+        }
+    }
+}
